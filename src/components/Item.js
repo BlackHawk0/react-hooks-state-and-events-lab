130a9463df148b1c2914d994ea6847ffa576c444
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-09-26 00:18:13 +0300

    states and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  console.log(name);
+  
+  const [addToCart, setAddToCart] = useState(false)
+  const [inCart, setInCart] = useState(false)
+
+  const cartModes = addToCart ? 'Add to Cart' : 'Remove From Cart'
+  const listClass = inCart ? "in-cart" : ""
+
   return (
-    <li className="">
+    <li className={listClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => {
+        setAddToCart(!addToCart)
+        setInCart(!inCart)
+        }}>{cartModes}</button>
     </li>
   );
 }
